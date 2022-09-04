697e6a236a65380df1dd300634e098f196ddcaa2
Johnny Chai/WYHQ/Wiwynn <Johnny_Chai@wiwynn.com>
2022-09-04 22:52:42 +0800

    20220904-2300

---
+++ Project1/Database/DB.sql
@@ -0,0 +1,17 @@
+-- DROP TABLE Department;
+
+-- Create Database mytestdb;
+
+-- Create Table dbo.Department(
+-- 	DepartmentId int identity(1,1),
+-- 	DepartmentName nvarchar(500)
+-- );
+
+Use mytestdb;
+
+-- Insert into Department values(1,'IT');
+-- Insert into Department values(2,'support');
+select * from Department;
+
+-- Insert into Employee values('Sam' , 'IT' , '2022-09-04' , 'anonymous.png');
+-- select * from Employee;
--- Readme.md
+++ Readme.md
@@ -1,8 +1,9 @@
-# Project about use different framework to buile FullStack Application
+# Project about use different framework to build FullStack Application
 
 > Project1
 ```
 Frontend : ReactJS
 Backend : Web API (version : .Net Core 3.1 )
 Database : SQL Server
+Reference : https://www.youtube.com/watch?v=ON-Z1iD6Y-c
 ```
