7ee2a730f220635969fabf9c4571d904ab952089
Johnny Chai/WYHQ/Wiwynn <Johnny_Chai@wiwynn.com>
2022-09-05 00:24:10 +0800

    20220905-0030

--- Project1/Database/DB.sql
+++ Project1/Database/DB.sql
@@ -3,8 +3,16 @@
 -- Create Database mytestdb;
 
 -- Create Table dbo.Department(
--- 	DepartmentId int identity(1,1),
+-- 	DepartmentId int ,
 -- 	DepartmentName nvarchar(500)
+-- );
+
+-- Create Table dbo.Employee(
+-- 	EmployeeId int ,
+-- 	EmployeeName nvarchar(500),
+-- 	Department nvarchar(500),
+-- 	DateOfJoining datetime,
+-- 	PhotoFileName nvarchar(500),
 -- );
 
 Use mytestdb;
@@ -13,5 +21,6 @@ Use mytestdb;
 -- Insert into Department values(2,'support');
 select * from Department;
 
--- Insert into Employee values('Sam' , 'IT' , '2022-09-04' , 'anonymous.png');
--- select * from Employee;
+-- Insert into Employee values(1 , 'Johnny' , 'IT' , GETDATE() , 'anonymous.png');
+-- Insert into Employee values(2 , 'David' , 'Pharmacist' , GETDATE() , 'someone.png');
+ select * from Employee;
